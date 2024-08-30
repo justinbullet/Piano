09823d8b53a8adbe0f932aee04f9976b625bf27a
pyrow0luv <thaivp853@outlook.com>
2024-08-30 13:54:15 +0700

    Them class

--- src/Piano.java
+++ src/Piano.java
@@ -1,6 +1,6 @@
 import org.jfugue.player.Player;
 
-public class HelloWorld {
+public class Piano {
   public static void main(String[] args) {
     Player player = new Player();
     player.play("C D E F G A B C6");
